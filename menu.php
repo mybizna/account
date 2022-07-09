793047f84ff9060b9546431271f88715a0c141a0
Dedan Irungu <dedanirungu@gmail.com>
2022-07-09 16:06:41 +0300

    changes to menu list

--- menu.php
+++ menu.php
@@ -11,7 +11,7 @@ $this->add_module_info("account", [
 //$this->add_menu("module", "key", "title","path", "icon", "position");
 $this->add_menu("account", "dashboard", "Dashboard", "/dashboard", "fas fa-cogs", 1);
 $this->add_menu("account", "transaction", "Transaction", "/account/admin/transaction", "fas fa-cogs", 1);
-$this->add_menu("account", "invoice", "Invoice", "/account/admin/invoices", "fas fa-cogs", 1);
+$this->add_menu("account", "invoice", "Invoice", "/account/admin/invoice", "fas fa-cogs", 1);
 $this->add_menu("account", "coupon", "Coupon", "/account/admin/coupon", "fas fa-cogs", 1);
 $this->add_menu("account", "gateway", "Gateway", "/account/admin/gateway", "fas fa-cogs", 1);
 $this->add_menu("account", "payment", "Payment", "/account/admin/payment", "fas fa-cogs", 1);
@@ -21,8 +21,11 @@ $this->add_menu("account", "reports", "Reports", "/reports", "fas fa-cogs", 5);
 
 //$this->add_submenu("module", "key", "title","path", "position");
 
-$this->add_submenu("account", "setting", "Financial Year", "/account/admin/Financial Year", 5);
+$this->add_submenu("account", "setting", "Gateway", "/account/admin/gateway", 5);
+$this->add_submenu("account", "setting", "", "", 5);
+$this->add_submenu("account", "setting", "Financial Year", "/account/admin/financial_year", 5);
 $this->add_submenu("account", "setting", "Chart of Accounts", "/account/admin/chart_of_account", 5);
+$this->add_submenu("account", "setting", "", "", 5);
 $this->add_submenu("account", "setting", "Rate", "/account/admin/rate", 5);
 $this->add_submenu("account", "setting", "Ledger", "/account/admin/ledger", 5);
 $this->add_submenu("account", "setting", "Ledger Category", "/account/admin/ledger_category", 5);
