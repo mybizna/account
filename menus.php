0b0dd48098ddcad27c2ff7b48e60457cfd49862a
Dedan Irungu <dedanirungu@gmail.com>
2022-09-14 12:14:34 +0300

    Update

--- menus.php
+++ menus.php
@@ -4,12 +4,11 @@ $this->add_module_info("account", [
     'title' => 'Accounting',
     'description' => 'Accounting',
     'icon' => 'fas fa-funnel-dollar',
-    'path' => '/transactions',
+    'path' => '/account/admin/invoice',
     'class_str'=> 'text-primary border-primary'
 ]);
 
 //$this->add_menu("module", "key", "title","path", "icon", "position");
-$this->add_menu("account", "dashboard", "Dashboard", "/dashboard", "fas fa-cogs", 1);
 $this->add_menu("account", "invoice", "Invoice", "/account/admin/invoice", "fas fa-cogs", 1);
 $this->add_menu("account", "payment", "Payment", "/account/admin/payment", "fas fa-cogs", 1);
 $this->add_menu("account", "transaction", "Transaction", "/account/admin/transaction", "fas fa-cogs", 1);
