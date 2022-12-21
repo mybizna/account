d49a3c597e7eae480e7749fdccf3b6ed9b72cd3d
Dedan Irungu <dedanirungu@gmail.com>
2022-12-21 15:53:25 +0300

    sdfsdf

--- menus.php
+++ menus.php
@@ -7,7 +7,7 @@ $this->add_module_info("account", [
     'path' => '/account/admin/invoice',
     'class_str'=> 'text-primary border-primary'
 ]);
-
+ 
 
 //$this->add_menu("module", "key", "title","path", "icon", "position");
 $this->add_menu("account", "invoice", "Invoice", "/account/admin/invoice", "fas fa-cogs", 1);
