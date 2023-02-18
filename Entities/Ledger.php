8b316ee5ddc0bf28d853a0120348f68620ee4fcc
Dedan Irungu <dedanirungu@gmail.com>
2023-02-18 16:10:25 +0300

    Changes to add system

--- Entities/Ledger.php
+++ Entities/Ledger.php
@@ -2,9 +2,9 @@
 
 namespace Modules\Account\Entities;
 
-use Modules\Base\Entities\BaseModel;
 use Illuminate\Database\Schema\Blueprint;
 use Modules\Base\Classes\Migration;
+use Modules\Base\Entities\BaseModel;
 
 class Ledger extends BaseModel
 {
@@ -27,8 +27,8 @@ class Ledger extends BaseModel
         $table->string('name')->nullable();
         $table->string('slug')->nullable();
         $table->integer('code')->nullable();
-        $table->tinyInteger('unused')->nullable();
-        $table->tinyInteger('system')->nullable();
+        $table->tinyInteger('unused')->default(true);
+        $table->tinyInteger('system')->default(false);
     }
 
     public function post_migration(Blueprint $table)
@@ -39,5 +39,25 @@ class Ledger extends BaseModel
         if (Migration::checkKeyExist('account_ledger', 'category_id')) {
             $table->foreign('category_id')->references('id')->on('account_ledger_category')->nullOnDelete();
         }
+    }
+
+    public function deleteRecord($id)
+    {
+
+        $ledger = $this->where('id', $id)->first();
+
+        if ($ledger->system) {
+            return [
+                'module' => $this->module,
+                'model' => $this->model,
+                'status' => 0,
+                'error' => 1,
+                'record' => [],
+                'message' => 'You can not Delete a Lerger Set by system.',
+            ];
+        }
+
+        parent::deleteRecord($id);
+
     }
 }
