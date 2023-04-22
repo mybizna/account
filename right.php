4e445f64050983c56da09f409ec800f2d07b395b
Dedan Irungu <dedanirungu@gmail.com>
2023-04-22 15:17:12 +0300

    Update rights

--- right.php
+++ right.php
@@ -1,15 +1,53 @@
 <?php
-/*
-account,invoice,administrator,1,1,1,1
-account,invoice,manager,1,1,1,1
-account,invoice,supervisor,1,1,1,1
-account,invoice,staff,1,1,1,0
-account,invoice,registered,1,1,0,0
-account,invoice,guest,1,0,0,0
 
-account_invoice_edit=Yes
+$this->add_right("account", "chart_of_account", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("account", "chart_of_account", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("account", "chart_of_account", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("account", "chart_of_account", "staff", view:true, add:true, edit:true);
+$this->add_right("account", "chart_of_account", "registered", view:true, add:true);
+$this->add_right("account", "chart_of_account", "guest", view:true, );
 
- */
+$this->add_right("account", "coupon", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("account", "coupon", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("account", "coupon", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("account", "coupon", "staff", view:true, add:true, edit:true);
+$this->add_right("account", "coupon", "registered", view:true, add:true);
+$this->add_right("account", "coupon", "guest", view:true, );
+
+$this->add_right("account", "financial_year", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("account", "financial_year", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("account", "financial_year", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("account", "financial_year", "staff", view:true, add:true, edit:true);
+$this->add_right("account", "financial_year", "registered", view:true, add:true);
+$this->add_right("account", "financial_year", "guest", view:true, );
+
+$this->add_right("account", "gateway", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("account", "gateway", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("account", "gateway", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("account", "gateway", "staff", view:true, add:true, edit:true);
+$this->add_right("account", "gateway", "registered", view:true, add:true);
+$this->add_right("account", "gateway", "guest", view:true, );
+
+$this->add_right("account", "gateway_allowedin", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("account", "gateway_allowedin", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("account", "gateway_allowedin", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("account", "gateway_allowedin", "staff", view:true, add:true, edit:true);
+$this->add_right("account", "gateway_allowedin", "registered", view:true, add:true);
+$this->add_right("account", "gateway_allowedin", "guest", view:true, );
+
+$this->add_right("account", "gateway_disallowedin", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("account", "gateway_disallowedin", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("account", "gateway_disallowedin", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("account", "gateway_disallowedin", "staff", view:true, add:true, edit:true);
+$this->add_right("account", "gateway_disallowedin", "registered", view:true, add:true);
+$this->add_right("account", "gateway_disallowedin", "guest", view:true, );
+
+$this->add_right("account", "gateway_rate", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("account", "gateway_rate", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("account", "gateway_rate", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("account", "gateway_rate", "staff", view:true, add:true, edit:true);
+$this->add_right("account", "gateway_rate", "registered", view:true, add:true);
+$this->add_right("account", "gateway_rate", "guest", view:true, );
 
 $this->add_right("account", "invoice", "administrator", view:true, add:true, edit:true, delete:true);
 $this->add_right("account", "invoice", "manager", view:true, add:true, edit:true, delete:true);
@@ -17,3 +55,108 @@ $this->add_right("account", "invoice", "supervisor", view:true, add:true, edit:t
 $this->add_right("account", "invoice", "staff", view:true, add:true, edit:true);
 $this->add_right("account", "invoice", "registered", view:true, add:true);
 $this->add_right("account", "invoice", "guest", view:true, );
+
+$this->add_right("account", "invoice_coupon", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("account", "invoice_coupon", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("account", "invoice_coupon", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("account", "invoice_coupon", "staff", view:true, add:true, edit:true);
+$this->add_right("account", "invoice_coupon", "registered", view:true, add:true);
+$this->add_right("account", "invoice_coupon", "guest", view:true, );
+
+$this->add_right("account", "invoice_item", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("account", "invoice_item", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("account", "invoice_item", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("account", "invoice_item", "staff", view:true, add:true, edit:true);
+$this->add_right("account", "invoice_item", "registered", view:true, add:true);
+$this->add_right("account", "invoice_item", "guest", view:true, );
+
+$this->add_right("account", "invoice_item_rate", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("account", "invoice_item_rate", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("account", "invoice_item_rate", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("account", "invoice_item_rate", "staff", view:true, add:true, edit:true);
+$this->add_right("account", "invoice_item_rate", "registered", view:true, add:true);
+$this->add_right("account", "invoice_item_rate", "guest", view:true, );
+
+$this->add_right("account", "invoice_status", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("account", "invoice_status", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("account", "invoice_status", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("account", "invoice_status", "staff", view:true, add:true, edit:true);
+$this->add_right("account", "invoice_status", "registered", view:true, add:true);
+$this->add_right("account", "invoice_status", "guest", view:true, );
+
+$this->add_right("account", "journal", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("account", "journal", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("account", "journal", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("account", "journal", "staff", view:true, add:true, edit:true);
+$this->add_right("account", "journal", "registered", view:true, add:true);
+$this->add_right("account", "journal", "guest", view:true, );
+
+$this->add_right("account", "ledger", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("account", "ledger", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("account", "ledger", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("account", "ledger", "staff", view:true, add:true, edit:true);
+$this->add_right("account", "ledger", "registered", view:true, add:true);
+$this->add_right("account", "ledger", "guest", view:true, );
+
+$this->add_right("account", "ledger_category", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("account", "ledger_category", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("account", "ledger_category", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("account", "ledger_category", "staff", view:true, add:true, edit:true);
+$this->add_right("account", "ledger_category", "registered", view:true, add:true);
+$this->add_right("account", "ledger_category", "guest", view:true, );
+
+$this->add_right("account", "opening_balance", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("account", "opening_balance", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("account", "opening_balance", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("account", "opening_balance", "staff", view:true, add:true, edit:true);
+$this->add_right("account", "opening_balance", "registered", view:true, add:true);
+$this->add_right("account", "opening_balance", "guest", view:true, );
+
+$this->add_right("account", "payment", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("account", "payment", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("account", "payment", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("account", "payment", "staff", view:true, add:true, edit:true);
+$this->add_right("account", "payment", "registered", view:true, add:true);
+$this->add_right("account", "payment", "guest", view:true, );
+
+$this->add_right("account", "payment_rate", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("account", "payment_rate", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("account", "payment_rate", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("account", "payment_rate", "staff", view:true, add:true, edit:true);
+$this->add_right("account", "payment_rate", "registered", view:true, add:true);
+$this->add_right("account", "payment_rate", "guest", view:true, );
+
+$this->add_right("account", "rate", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("account", "rate", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("account", "rate", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("account", "rate", "staff", view:true, add:true, edit:true);
+$this->add_right("account", "rate", "registered", view:true, add:true);
+$this->add_right("account", "rate", "guest", view:true, );
+
+$this->add_right("account", "rate_allowedin", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("account", "rate_allowedin", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("account", "rate_allowedin", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("account", "rate_allowedin", "staff", view:true, add:true, edit:true);
+$this->add_right("account", "rate_allowedin", "registered", view:true, add:true);
+$this->add_right("account", "rate_allowedin", "guest", view:true, );
+
+$this->add_right("account", "rate_disallowedin", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("account", "rate_disallowedin", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("account", "rate_disallowedin", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("account", "rate_disallowedin", "staff", view:true, add:true, edit:true);
+$this->add_right("account", "rate_disallowedin", "registered", view:true, add:true);
+$this->add_right("account", "rate_disallowedin", "guest", view:true, );
+
+$this->add_right("account", "rate_file", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("account", "rate_file", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("account", "rate_file", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("account", "rate_file", "staff", view:true, add:true, edit:true);
+$this->add_right("account", "rate_file", "registered", view:true, add:true);
+$this->add_right("account", "rate_file", "guest", view:true, );
+
+$this->add_right("account", "transaction", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("account", "transaction", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("account", "transaction", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("account", "transaction", "staff", view:true, add:true, edit:true);
+$this->add_right("account", "transaction", "registered", view:true, add:true);
+$this->add_right("account", "transaction", "guest", view:true, );
