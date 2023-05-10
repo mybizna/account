7ec8e426f4aff4c514bd520f06cf6ce0b0f5cbc9
Dedan Irungu <dedanirungu@gmail.com>
2023-05-10 14:58:18 +0300

    Improvement to language transactions

--- settings.php
+++ settings.php
@@ -4,10 +4,8 @@ use Modules\Core\Classes\Currency;
 use Modules\Account\Classes\Ledger;
 
 $ledger = new Ledger();
-$currency = new Currency();
 
 $sales_revenue_id = $ledger->getLedgerId('sales_revenue', true);
-$currency_id = $currency->getCurrencyId('USD');
 
 return [
     'sales_default_ledger' => [
@@ -22,16 +20,5 @@ return [
 
         ],
     ],
-    'sales_default_currency' => [
-        "title" => "Sales Default Currency",
-        "type" => "recordpicker",
-        "value" => $currency_id,
-        "comp_url" => "core/admin/currency/list.vue",
-        "setting" => [
-            'path_param' => ["core", "currency"],
-            'fields' => ['name', 'code'],
-            'template' => '[name] ([code])',
-
-        ],
-    ],
+ 
 ];
