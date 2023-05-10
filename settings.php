545781de80c12a72ee29be02a394d03e6dea8f87
Dedan Irungu <dedanirungu@gmail.com>
2023-05-10 10:37:13 +0300

    Improving Currency

--- settings.php
+++ settings.php
@@ -1,10 +1,13 @@
 <?php
 
+use Modules\Core\Classes\Currency;
 use Modules\Account\Classes\Ledger;
 
 $ledger = new Ledger();
+$currency = new Currency();
 
 $sales_revenue_id = $ledger->getLedgerId('sales_revenue', true);
+$currency_id = $currency->getCurrencyId('USD');
 
 return [
     'sales_default_ledger' => [
@@ -16,6 +19,18 @@ return [
             'path_param' => ["account", "ledger"],
             'fields' => ['name', 'slug', 'chart_id__account_chart_of_account__name'],
             'template' => '[name] ([slug]) - [chart_id__account_chart_of_account__name]',
+
+        ],
+    ],
+    'sales_default_currency' => [
+        "title" => "Sales Default Currency",
+        "type" => "recordpicker",
+        "value" => $currency_id,
+        "comp_url" => "core/admin/currency/list.vue",
+        "setting" => [
+            'path_param' => ["core", "currency"],
+            'fields' => ['name', 'code'],
+            'template' => '[name] ([code])',
 
         ],
     ],
