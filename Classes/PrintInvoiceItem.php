05a1149044b8ab7c2cb380cf522e41c4723f8acd
Dedan Irungu <dedanirungu@gmail.com>
2023-05-23 12:04:30 +0300

    completion of queuing system

---
+++ Classes/PrintInvoiceItem.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Modules\Queuing\Classes;
+
+/* A wrapper to do organise item names & prices into columns */
+
+class Item
+{
+    private $name;
+    private $price;
+    private $dollarSign;
+
+    public function __construct($name = '', $price = '', $dollarSign = false)
+    {
+        $this->name = $name;
+        $this->price = $price;
+        $this->dollarSign = $dollarSign;
+    }
+
+    public function getAsString($width = 48)
+    {
+        $rightCols = 10;
+        $leftCols = $width - $rightCols;
+        if ($this->dollarSign) {
+            $leftCols = $leftCols / 2 - $rightCols / 2;
+        }
+        $left = str_pad($this->name, $leftCols);
+
+        $sign = ($this->dollarSign ? '$ ' : '');
+        $right = str_pad($sign . $this->price, $rightCols, ' ', STR_PAD_LEFT);
+        return "$left$right\n";
+    }
+
+    public function __toString()
+    {
+        return $this->getAsString();
+    }
+
+}
