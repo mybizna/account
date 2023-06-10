6c554d267141b67e2b742966d53aa91111e899e7
Dedan Irungu <dedanirungu@gmail.com>
2023-06-10 12:00:21 +0300

    Adding testing code

---
+++ Tests/TestCase.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Modules\Account\Tests;
+
+use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+
+abstract class TestCase extends BaseTestCase
+{
+    use CreatesApplication;
+}
