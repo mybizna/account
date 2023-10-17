f66a4b2e5eeaeab58c20180225cc622b46195ea1
Dedan <dedanirungu@gmail.com>
2023-10-17 10:45:17 +0300

    change module accounting from using laravel modular to custom structure

--- Routes/api.php
+++ Routes/api.php
@@ -2,12 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:sanctum')->group(function () {
-    Route::get('chart_of_account/recordselect', 'ChartOfAccountController@recordselect');
-    Route::get('ledger/recordselect', 'LedgerController@recordselect');
-    Route::get('rate/recordselect', 'RateController@recordselect');
-    Route::get('invoice/fetchdata', 'InvoiceController@fetchData');
-    Route::post('invoice/savedata', 'InvoiceController@saveData');
-    Route::get('invoice/reconcileinvoices', 'InvoiceController@reconcileInvoices');
-    Route::get('chart_of_account/summation/{slug}', 'ChartOfAccountController@chartsummary');
-});
+$rights = ["account_invoice_edit", "account_invoice_edit_own", "account_invoice_add", "account_invoice_add_own"];
+
+Route::get('chart_of_account/recordselect', 'ChartOfAccountController@recordselect', )->rights($rights);
+Route::get('ledger/recordselect', 'LedgerController@recordselect', )->rights($rights);
+Route::get('rate/recordselect', 'RateController@recordselect', )->rights($rights);
+Route::get('invoice/fetchdata', 'InvoiceController@fetchData', )->rights($rights);
+Route::post('invoice/savedata', 'InvoiceController@saveData', )->rights($rights);
+Route::get('invoice/reconcileinvoices', 'InvoiceController@reconcileInvoices', )->rights($rights);
+Route::get('chart_of_account/summation/{slug}', 'ChartOfAccountController@chartsummary', )->rights($rights);
