1300beb398f2fe7db28da01ae0eaf9032c2d7f68
Dedan <dedanirungu@gmail.com>
2023-10-18 15:40:45 +0300

    Release 23.10.002

--- Routes/web.php
+++ Routes/web.php
@@ -9,6 +9,6 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
- */
+*/
 
 Route::get('/payment/{invoice_id}', 'PaymentController@payment')->name('account_payment');
