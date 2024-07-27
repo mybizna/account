0bd393a416ae6cd9c8162ba3241404d2d967a8e5
Dedan <dedanirungu@gmail.com>
2024-07-27 20:02:48 +0300

    chore: Add Filament AccountPlugin and Cluster classes

---
+++ Filament/AccountPlugin.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Modules\Account\Filament;
+
+use Coolsam\Modules\Concerns\ModuleFilamentPlugin;
+use Filament\Contracts\Plugin;
+use Filament\Panel;
+
+class AccountPlugin implements Plugin
+{
+    use ModuleFilamentPlugin;
+
+    public function getModuleName(): string
+    {
+        return 'Account';
+    }
+
+    public function getId(): string
+    {
+        return 'account';
+    }
+
+    public function boot(Panel $panel): void
+    {
+        // TODO: Implement boot() method.
+    }
+}
